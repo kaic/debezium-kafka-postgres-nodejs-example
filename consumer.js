41d4f855c075a2baaa5832a76059f364d5d9ee4d
Kaic Bento <kaicbento@outlook.com>
2021-04-23 16:38:14 -0300

    update worker to only use postgres

--- consumer.js
+++ consumer.js
@@ -1,89 +1,86 @@
 require('dotenv').config()
 const { Pool } = require("pg");
-const logger = require('./logger')
-const { Kafka } = require("kafkajs");
 
-const brokers = [process.env.BROKER_ENDPOINT_1, process.env.BROKER_ENDPOINT_2, process.env.BROKER_ENDPOINT_3]
+const postgresPool = new Pool({
+  user: process.env.PG_USER,
+  database: process.env.PG_DB,
+  password: process.env.PG_PASS,
+  port: process.env.PG_PORT,
+  host: process.env.PG_HOST,
+  keepAlive: true,
+  max: 60,
+});
+
+const TABLE_NAME = "BalanceOperations"
+const NEW_ID_COLUMN_NAME = "id_bigint"
+const BATCH_SIZE = Number(process.env.BATCH_SIZE) || 100
+let offset = Number(process.env.OFFSET) || 0
+let count = 0
+let sum = 0
 
 async function run () {
 
-  logger.info('Worker is starting')
-
-  const kafka = new Kafka({
-    brokers,
-    clientId: "consumer-worker",
-  });
-
-  const postgresPool = new Pool({
-    user: process.env.PG_USER,
-    database: process.env.PG_DB,
-    password: process.env.PG_PASS,
-    port: process.env.PG_PORT,
-    host: process.env.PG_HOST,
-    keepAlive: true,
-    max: 50,
-  });
-
-  const TABLE_NAME = "BalanceOperations";
-  const NEW_ID_COLUMN_NAME = "id_bigint";
+  console.log('WORKER JOB IS STARTING')
 
   await postgresPool
     .query("SELECT NOW() as now")
-    .then((_) => logger.info('Worker has connnected to Postgres'))
+    .then((_) => console.log('WORKER HAS CONNETED TO POSTGRES'))
     .catch(console.error);
 
-  const consumer = kafka.consumer({ groupId: "kafka-connect" });
+  const timetaken = "TIME TAKEN BY THE WORKER TO COMPLETE THE JOB";
+  console.time(timetaken);
 
-  await consumer.connect();
+  const postgresConnection = await postgresPool.connect();  
 
-  await consumer.subscribe({ topic: process.env.KAFKA_TOPIC });
-
-  logger.info('Worker ready to go!')
-
-  const postgresConnection = await postgresPool.connect();
-
-  const { rows, count } = await postgresConnection.query(
-    `SELECT COUNT(1) from "${TABLE_NAME}" where ${NEW_ID_COLUMN_NAME} is not null`
+  const { rows } = await postgresConnection.query(
+    `SELECT COUNT(1) from "${TABLE_NAME}" where ${NEW_ID_COLUMN_NAME} is null`
   );
 
-  logger.info(`There are ${count} rows to update`)
+  count = rows[0].count
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const parsedMessage = JSON.parse(message.value.toString());
-      const row = parsedMessage.payload.after;
-
-      logger.info('Message Incoming', parsedMessage)
-
-      const postgresConnection = await postgresPool.connect();
-
-      try {
-        await postgresConnection.query("BEGIN");
-
-        await postgresConnection.query(
-          "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"
-        );
-
-        if(!row.id_bigint) {
-          const {
-            rows,
-          } = await postgresConnection.query(
-            `UPDATE "${TABLE_NAME}" set ${NEW_ID_COLUMN_NAME} = $1 WHERE id = $1 RETURNING *`,
-            [row.id]
-          );
+  console.log(`WORKER HAS ${count} ROWS TO UPDATED`)
   
-          logger.info(`Row ${row.id} updated`, rows)
-        }
+  let batch = []
+  while (offset <= count) {
+    const { rows } = await postgresConnection.query(
+      `SELECT * from "${TABLE_NAME}" where ${NEW_ID_COLUMN_NAME} is null LIMIT ${BATCH_SIZE} OFFSET ${offset}`
+    );
 
-        await postgresConnection.query("COMMIT");
-      } catch (e) {
-        await postgresConnection.query("ROLLBACK");
-        throw e;
-      } finally {
-        postgresConnection.release();
-      }
-    },
-  });
+    batch = rows
+    
+    await batch.map(updateRow)
+    offset += BATCH_SIZE
+  }
+
+  console.timeEnd(timetaken);
+  console.log(`WORKER JOB FINISHED`)
 }
 
-(async () => run())();
+async function updateRow(row) {
+  const postgresConnection = await postgresPool.connect();
+
+  try {
+    await postgresConnection.query("BEGIN")
+
+    if(!row.id_bigint) {
+      const {
+        rows,
+      } = await postgresConnection.query(
+        `UPDATE "${TABLE_NAME}" set ${NEW_ID_COLUMN_NAME} = $1 WHERE id = $1 RETURNING *`,
+        [row.id]
+      );
+      
+      sum++
+      console.log(`[${count}/${sum}] UPDATED ROWS - ID ${row.id}`)
+      await postgresConnection.query("COMMIT");
+    }
+
+  } catch (e) {
+    await postgresConnection.query("ROLLBACK");
+    throw e;
+  } finally {
+    await postgresConnection.release();
+  }
+}
+
+(() => run())();
--- start_worker.sh
+++ start_worker.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo "sleeping and waiting for kafka connect"
-sleep 20s
 echo "starting worker..."
 node consumer.js
