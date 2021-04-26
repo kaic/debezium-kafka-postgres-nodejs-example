5636687e6d7ed5eb9a6bee606d851ae3b5873b85
Kaic Bento <kaicbento@outlook.com>
2021-04-26 19:58:38 -0300

    fix concurrency and offset thing

--- consumer.js
+++ consumer.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const { Pool } = require("pg")
 const Promise = require('bluebird')
 
-let pgConnection = null
 const pgPool = new Pool({
   user: process.env.PG_USER,
   database: process.env.PG_DB,
@@ -11,17 +10,18 @@ const pgPool = new Pool({
   host: process.env.PG_HOST,
   keepAlive: true,
   statement_timeout: 300000,
-  max: 3
+  max: 90
 })
+let pgConnection = null
 
 const TABLE_NAME = "BalanceOperations"
 const NEW_ID_COLUMN_NAME = "id_bigint"
 const BATCH_SIZE = Number(process.env.BATCH_SIZE) || 1000
 const CONCURRENCY = Number(process.env.CONCURRENCY) || 100
-let offset = Number(process.env.OFFSET) || 0
 let count = Number(process.env.COUNT) || 0
 let rowsUpdated = 0
 let rowsNotUpdated = 0
+let processeds = 0
 
 function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -29,16 +29,14 @@ function delay(ms) {
 
 async function run() {
 
-  console.log('WORKER JOB IS STARTING')
-  console.log('PARAMS')
-  console.log({ TABLE_NAME, NEW_ID_COLUMN_NAME, BATCH_SIZE, CONCURRENCY, OFFSET: process.env.OFFSET, COUNT: process.env.COUNT })
+  console.log('WORKER JOB IS STARTING WITH PARAMS', { TABLE_NAME, NEW_ID_COLUMN_NAME, BATCH_SIZE, CONCURRENCY, COUNT: count })
 
-  pgConnection = await pgPool.connect()
   await pgPool
     .query("SELECT NOW() as now")
     .then((_) => console.log('WORKER HAS CONNETED TO POSTGRES'))
     .catch(console.error)
 
+  pgConnection = await pgPool.connect()  
 
   const workerJobTimetaken = "TIME TAKEN BY THE WORKER TO COMPLETE THE JOB"
   console.time(workerJobTimetaken)
@@ -48,41 +46,36 @@ async function run() {
   )
 
   count = Number(rows[0].count)
-
   console.log(`WORKER HAS ${count} ROWS TO UPDATED`)
 
   await delay(1000)
 
   //retirar do while
-  //salvar offset no banco
+  //salvar infos no banco
   //salvar batchs não processados no banco
-  let batchNumber = 0
-  while (offset < count) {
-    await processBatch(batchNumber, offset)
-    batchNumber++
-    offset += BATCH_SIZE
-    console.log({ offset, count })
+  while (processeds < count) {
+    await processBatch()
   }
 
   console.timeEnd(workerJobTimetaken)
-  console.log(`ROWS NOT UPDATED - ${rowsNotUpdated}`)
-  console.log(`ROWS UPDATED - ${rowsUpdated}`)
-  console.log(`WORKER JOB FINISHED`)
+  console.log(`WORKER JOB FINISHED`, {
+    rowsUpdated,
+    rowsNotUpdated
+  })
+
+  process.exit(0)
 }
 
-async function processBatch(batchNumber, _offset, retryAttempt = 0) {
-  console.log({ batchNumber, _offset, retryAttempt })
-  console.time("TIME TAKEN TO PROCESS BATCH")
+async function processBatch(retryAttempt = 0) {
+  const batchNumber = processeds / BATCH_SIZE
 
   if (retryAttempt > 3) {
-    console.log(`BATCH ${batchNumber} EXCEEDED RETRY ATTEMPTS - SKIPPING FOR OFFSET ${_offset}`)
+    console.log(`BATCH ${batchNumber} EXCEEDED RETRY ATTEMPTS - SKIPPING`)
     return
   }
 
   try {
-    const { rows } = await pgConnection.query(
-      `SELECT * from "${TABLE_NAME}" where ${NEW_ID_COLUMN_NAME} is null order by id asc LIMIT ${BATCH_SIZE} OFFSET ${_offset}`
-    )
+    const { rows } = await pgConnection.query(`SELECT * from "${TABLE_NAME}" where ${NEW_ID_COLUMN_NAME} is null order by id asc LIMIT ${BATCH_SIZE}`)
 
     await Promise.map(rows, updateRow, {
       concurrency: CONCURRENCY
@@ -93,10 +86,10 @@ async function processBatch(batchNumber, _offset, retryAttempt = 0) {
     console.log(`RETRYING PROCESSING FOR BATCH ${batchNumber}`)
 
     retryAttempt++
-    await processBatch(batchNumber, _offset, retryAttempt)
+    await processBatch(retryAttempt)
   }
 
-  console.timeEnd("TIME TAKEN TO PROCESS BATCH")
+  
 }
 
 async function updateRow(params) {
@@ -110,24 +103,27 @@ async function updateRow(params) {
   }
 
   try {
-    if (!row.id_bigint) {
-      await pgConnection.query("BEGIN")
+    await pgConnection.query("BEGIN")
 
-      await pgConnection.query(
-        `UPDATE "${TABLE_NAME}" set ${NEW_ID_COLUMN_NAME} = $1 WHERE id = $1`,
-        [row.id]
-      )
+    await pgConnection.query(
+      `UPDATE "${TABLE_NAME}" set ${NEW_ID_COLUMN_NAME} = $1 WHERE id = $1`,
+      [row.id]
+    )
 
-      await pgConnection.query("COMMIT")
-      rowsUpdated++
-      console.log(`SUCCESS ON PROCESSING ROW ID ${row.id} ON ATTEMPT ${retryAttempt} | UPDATEDS [${count}/${rowsUpdated}]`)
-    }
+    await pgConnection.query("COMMIT")
+    rowsUpdated++
+    console.log(`SUCCESS ON PROCESSING ROW ID ${row.id} ON ATTEMPT ${retryAttempt} | UPDATEDS [${count}/${rowsUpdated}]`)
+
   } catch (error) {
     await pgConnection.query("ROLLBACK")
     console.log(`ERROR ON PROCESSING ROW ID ${row.id} - RETRYING`)
     retryAttempt++
     await updateRow({ row, retryAttempt })
+
   }
+
+  processeds++
+  
 }
 
 (async () => await run())()
